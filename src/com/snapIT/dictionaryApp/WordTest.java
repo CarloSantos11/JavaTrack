7b98bc7c5f351dc706242a75987dcbf9f03100a4
Carlo Santos <carlo@snapit.solutions>
2020-11-18 23:24:30 -0600

    looping dictionary
    Signed-off-by: Carlo Santos <carlo@snapit.solutions>

--- src/com/snapIT/dictionaryApp/WordTest.java
+++ src/com/snapIT/dictionaryApp/WordTest.java
@@ -15,13 +15,46 @@ package com.snapIT.dictionaryApp;
 //        - root :boolean
 
 
+import java.util.Scanner;
+
 public class WordTest {
     public static void main(String[] args) {
-        Word newWord = new Word("Apple", "This is a fruit from the fruit family", "English", true);
-        Word tagalogWord = new Word("Masipag", "Arduous, hardworking", "Tagalog", true);
+        Scanner input = new Scanner(System.in);
 
-        newWord.printInfo();
-        System.out.println();
-        tagalogWord.printInfo();
+        boolean promptUser = true;
+
+        while(promptUser) {
+            System.out.println("What's the word? or type ':p' to quit");
+            String word = input.nextLine();
+            System.out.println("Have a definition?");
+            String definition = input.nextLine();
+            System.out.println("Language?");
+            String language = input.nextLine();
+            System.out.println("Is this the root form of the word? true or false.");
+            String root = input.nextLine();
+
+            if(root.equals("true")) {
+                boolean rootParam = true;
+                System.out.println("Hammanayaaaah");
+            }
+
+            if( word.equals(":p")) {
+                System.out.println("Hiyoooooo");
+                break;
+            }
+
+        }
+
     }
 }
+
+
+//    Word newWord = new Word("Apple", "This is a fruit from the fruit family", "English", true);
+//    Word tagalogWord = new Word("Masipag", "Arduous, hardworking", "Tagalog", true);
+//
+//        newWord.printInfo();
+//        System.out.println();
+//        tagalogWord.printInfo();
+
+
+// Possible
